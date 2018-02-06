4112ec8e606c5b78d1be9be620216eeda921bdf9
David Weis <davidmichaelweis@gmail.com>
2018-02-06 03:10:28 +0100

    Add listener for stance update messages to hopper controller

--- hopper_controller/src/hexapod_controller.py
+++ hopper_controller/src/hexapod_controller.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import
 import rospy
 from geometry_msgs.msg import Twist
 from hexapod_gait_engine import GaitController
-from hexapod_ik_driver import IkDriver, Vector2
+from hexapod_ik_driver import IkDriver, Vector2, Vector3
 from dynamixel_driver import DynamixelDriver, search_usb_2_ax_port
 
 
@@ -18,9 +18,15 @@ class HexapodController(object):
         self.controller.direction = Vector2(twist.linear.x, twist.linear.y)
         self.controller.rotation = twist.angular.x
 
+    def update_stance(self, twist):
+        transform = Vector3(twist.linear.x, twist.linear.y, twist.linear.z)
+        rotation = Vector3(twist.angular.x, twist.angular.y, twist.angular.z)
+        self.controller.update_relaxed_position(transform=transform, rotation=rotation)
+
     def listener(self):
-        rospy.init_node('quadruped_controller', anonymous=True)
+        rospy.init_node('hopper_controller', anonymous=True)
         rospy.Subscriber("quadruped_command", Twist, self.update_direction)
+        rospy.Subscriber("hopper_stance_translate", Twist, self.update_stance)
         rospy.spin()
         self.controller.stop()
 

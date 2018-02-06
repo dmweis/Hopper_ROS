ab91ae93d0ed56b3de06448144c1eea60b950e1d
David Weis <davidmichaelweis@gmail.com>
2018-02-06 01:55:51 +0100

    Update test for body transformation

--- hopper_controller/src/hexapod_test.py
+++ hopper_controller/src/hexapod_test.py
@@ -1,5 +1,7 @@
+from __future__ import print_function
+from __future__ import print_function
 from hexapod_gait_engine import GaitController
-from hexapod_ik_driver import IkDriver, Vector2
+from hexapod_ik_driver import IkDriver, Vector2, Vector3
 from dynamixel_driver import DynamixelDriver, search_usb_2_ax_port
 from time import sleep
 
@@ -8,16 +10,19 @@ print("Start")
 servo_driver = DynamixelDriver("COM8")
 ik_driver = IkDriver(servo_driver)
 controller = GaitController(ik_driver)
+while not controller.ready:
+    print("waiting...")
+    sleep(1)
 print("Starting move")
-controller.direction = Vector2(6, 0)
-# controller.rotation = -10
-sleep(10)
-controller.rotation = 0
-controller.direction = Vector2(0, 0)
+
+print("Changing")
+controller.update_relaxed_position(transform=Vector3(0, 0, 5))
+sleep(2)
+controller.direction = Vector2(3, 0)
 sleep(5)
+controller.direction = Vector2(0, 0)
 
 print("Finished")
 
-controller.stop(True)
-raw_input("Press enter to exit")
+controller.stop()
 print("Exiting")

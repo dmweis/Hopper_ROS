938268ebb355b60f79a15795f0cb08a7377b60b7
David Weis <davidmichaelweis@gmail.com>
2018-04-16 23:47:48 +0200

    Fix path errer and string property name

--- hopper_speech/src/hopper_speech_node.py
+++ hopper_speech/src/hopper_speech_node.py
@@ -13,11 +13,11 @@ class HopperSpeechHandles(object):
         self.sound_client = SoundClient()
         self.play_sound_sub = rospy.Subscriber("hopper_play_sound", String, self.on_play_sound)
         rospack = rospkg.RosPack()
-        self.sound_path = rospack.get_path('hopper_speech') + "\\sounds\\"
+        self.sound_path = rospack.get_path('hopper_speech') + "/sounds/"
         rospy.spin()
 
     def on_play_sound(self, string):
-        self.sound_client.playWave(self.sound_path + string.text + ".ogg")
+        self.sound_client.playWave(self.sound_path + string.data + ".ogg")
 
 
 if __name__ == '__main__':

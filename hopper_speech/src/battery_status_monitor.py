1d5616be78678c9bbb19d3d872c6b4cf77fbc601
David Weis <davidmichaelweis@gmail.com>
2018-04-19 23:39:34 +0200

    Make sure battery warning is only issued once

--- hopper_speech/src/battery_status_monitor.py
+++ hopper_speech/src/battery_status_monitor.py
@@ -13,20 +13,20 @@ class BatteryStatusMonitor(object):
         rospy.init_node("hopper_battery_monitor")
         self.lowest_recorded_voltage = 15.0
         self.speech_publisher = rospy.Publisher('hopper_play_sound', String, queue_size=5)
-        self.telemetrics_sub = rospy.Subscriber("hopper_telemetrics", HexapodTelemetrics, self.on_new_telemetrics)
+        self.telemetrics_sub = rospy.Subscriber("hopper_telemetrics", HexapodTelemetrics, self.on_new_telemetrics, queue_size=1)
         rospy.spin()
 
     def on_new_telemetrics(self, message):
         voltages = [data.voltage for data in message.servos]
         mean_voltage = mean(voltages)
         if mean_voltage < self.lowest_recorded_voltage:
-            self.lowest_recorded_voltage = mean_voltage
-            if mean_voltage < 10.5:
+            if mean_voltage < 10.5 and self.lowest_recorded_voltage > 10.5:
                 self.speech_publisher.publish("battery_critical")
-            elif mean_voltage < 11:
+            elif mean_voltage < 11 and self.lowest_recorded_voltage > 11:
                 self.speech_publisher.publish("battery_below_11")
-            elif mean_voltage < 12:
+            elif mean_voltage < 12 and self.lowest_recorded_voltage > 12:
                 self.speech_publisher.publish("battery_below_12")
+            self.lowest_recorded_voltage = mean_voltage
 
 
 if __name__ == '__main__':

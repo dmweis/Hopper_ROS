c25ab60c5f50e2a0311dbf6a3614ef040bc89b12
David Weis <davidmichaelweis@gmail.com>
2018-05-13 14:34:13 +0200

    Add merger for odometry and update config for DWA planner

---
+++ hopper_navigation/src/odometry_merger.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from threading import Thread
+import rospy
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
+
+
+class OdometryMerger(Thread):
+    def __init__(self):
+        super(OdometryMerger, self).__init__()
+        rospy.init_node("odometry_merger")
+        self._merged_message = Odometry()
+        self._slam_subscriber = rospy.Subscriber("", Odometry, self._on_slam_odometry)
+        self._odometry_subscriber = rospy.Subscriber("Odom", Odometry, self._on_robot_odometry)
+        self._merged_odometry_publisher = rospy.Publisher("merged_odometry", Odometry, queue_size=10)
+
+    def _on_slam_odometry(self, slam_odom):
+        self._merged_message.header.frame_id = slam_odom.header.frame_id
+        self._merged_message.child_frame_id = slam_odom.child_frame_id
+        self._merged_message.pose.covariance = slam_odom.pose.covariance
+        self._merged_message.pose.pose = slam_odom.pose.pose
+
+    def _on_robot_odometry(self, robot_odom):
+        self._merged_message.pose.covariance = robot_odom.twist.covariance
+        self._merged_message.twist.twist = robot_odom.pose.twist.twist
+
+    def run(self):
+        rate = rospy.Rate(30)
+        while not rospy.is_shutdown():
+            self._merged_message.header.stamp = rospy.Time.now()
+            self._merged_odometry_publisher.publish(self._merged_message)
+            rate.sleep()

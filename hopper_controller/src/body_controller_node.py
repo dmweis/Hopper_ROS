68ad1b561715de657aa35433e902885f420bdc04
David Weis <davidmichaelweis@gmail.com>
2018-08-03 20:02:02 +0200

    Create separate motor cotnrol into a new node

---
+++ hopper_controller/src/body_controller_node.py
@@ -0,0 +1,42 @@
+import rospy
+
+from hopper_msgs.msg import ServoTelemetrics, HexapodTelemetrics
+from hopper_controller.msg import BodyMotorPositions, MotorCompSpeedCommand, TorqueCommand
+from dynamixel.dynamixel_driver import DynamixelDriver, search_usb_2_ax_port
+
+
+class BodyMotorController(object):
+    def __init__(self):
+        self.servo_driver = DynamixelDriver(search_usb_2_ax_port())
+        self.servo_ids = self.servo_driver.search_servos(0, 20)
+        rospy.Subscriber("hopper/body/motor_command", BodyMotorPositions, self.on_motor_command)
+        rospy.Subscriber("hopper/body/compliance_speed", MotorCompSpeedCommand, self.on_compliance_speed_command)
+        rospy.Subscriber("hopper/body/torque_command", TorqueCommand, self.on_torque_command)
+        self.telementrics_publisher = rospy.Publisher('hopper_telemetrics', HexapodTelemetrics, queue_size=5)
+        duration = rospy.Duration(4)
+        while not rospy.is_shutdown():
+            rospy.sleep(duration)
+            self.read_motor_telemetrics()
+
+    def on_motor_command(self, msg):
+        commands = map(lambda goal_command: (goal_command.servo_id, goal_command.goal_position), msg.goal_postions)
+        self.servo_driver.group_sync_write_goal_degrees(commands)
+
+    def on_compliance_speed_command(self, command):
+        self.servo_driver.set_compliance_slope(command.id, command.compliance_slope)
+        self.servo_driver.set_moving_speed(command.id, command.movement_speed)
+
+    def on_torque_command(self, command):
+        self.servo_driver.set_torque(command.servo_id, command.toruqe_on)
+
+    def read_motor_telemetrics(self):
+        robot_telemetrics = HexapodTelemetrics()
+        for servo_id in self.servo_ids:
+            voltage = self.servo_driver.read_voltage(servo_id)
+            temperature = self.servo_driver.read_temperature(servo_id)
+            robot_telemetrics.servos.append(ServoTelemetrics(servo_id, temperature, voltage))
+        self.telementrics_publisher.publish(robot_telemetrics)
+
+
+if __name__ == '__main__':
+    BodyMotorController()

ef3f021ec2960f9e69f4503a189b6a08e91b30cb
David Weis <davidmichaelweis@gmail.com>
2018-08-06 13:39:34 +0100

    Update keep_alive_node to prevent early shutdown

--- hopper_keep_alive/src/keep_alive_node.py
+++ hopper_keep_alive/src/keep_alive_node.py
@@ -8,12 +8,12 @@ class KeepAliveNode(object):
     def __init__(self):
         super(KeepAliveNode, self).__init__()
         rospy.init_node("keep_alive_node")
-        rospy.Service("halt", Halt, self.on_halt)
-        rospy.spin()
+        self.halt_service = rospy.Service("halt", Halt, self.on_halt)
+        self.halt_service.spin()
 
     def on_halt(self, _):
         rospy.loginfo("keep alive node shutting down")
-        rospy.signal_shutdown("Halt command received")
+        self.halt_service.shutdown()
         return HaltResponse()
 
 if __name__ == '__main__':

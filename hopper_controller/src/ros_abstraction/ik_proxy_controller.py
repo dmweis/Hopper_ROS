957476e74c0b817adfe4d41ef159d4c8b2b653be
David Weis <davidmichaelweis@gmail.com>
2018-08-11 23:28:39 +0100

    Return leg_pos message instead of motor read message

--- hopper_controller/src/ros_abstraction/ik_proxy_controller.py
+++ hopper_controller/src/ros_abstraction/ik_proxy_controller.py
@@ -8,7 +8,7 @@ class IkController(object):
     def __init__(self):
         super(IkController, self).__init__()
         rospy.wait_for_service("hopper/ik/read_leg_positions")
-        self.read_legs_service_proxy = rospy.ServiceProxy("hopper/ik/read_leg_positions", ReadHexapodLegPositions)
+        self.read_legs_service_proxy = rospy.ServiceProxy("hopper/ik/read_leg_positions", ReadHexapodLegPositions, persistent=True)
         self. move_legs_pub = rospy.Publisher("hopper/ik/move_legs", HexapodLegPositions, queue_size=5, latch=True)
         self.torque_publisher = rospy.Publisher("hopper/body/motor_torque", MotorTorque, queue_size=5, latch=True)
 
@@ -19,5 +19,5 @@ class IkController(object):
         self.move_legs_pub.publish(leg_positions)
 
     def read_current_leg_positions(self):
-        leg_positions_msg = self.read_legs_service_proxy()
+        leg_positions_msg = self.read_legs_service_proxy().leg_positions
         return LegPositions.ros_leg_positions_to_leg_positions(leg_positions_msg)

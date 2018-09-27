4d747ce474f9132739770d3724f18395cef847ee
David Weis <davidmichaelweis@gmail.com>
2018-09-27 06:03:01 +0100

    Fix play sounds on platform with non standard audio outputs

--- hopper_speech/src/hopper_speech_node.py
+++ hopper_speech/src/hopper_speech_node.py
@@ -3,10 +3,12 @@
 from os.path import expanduser
 from os import listdir
 import os.path as path
+
 import random
 import rospy
 import rospkg
-from sound_play.libsoundplay import SoundClient
+
+import pygame.mixer as sound
 from std_msgs.msg import String
 
 
@@ -14,10 +16,10 @@ class HopperSpeechHandles(object):
     def __init__(self):
         super(HopperSpeechHandles, self).__init__()
         rospy.init_node("hopper_speech_core", anonymous=True)
+        sound.init()
         rospack = rospkg.RosPack()
         self.primary_sound_path = rospack.get_path('hopper_speech') + "/sounds/"
         self.secondary_sound_path = expanduser("~") + "/Music/"
-        self.sound_client = SoundClient()
         rospy.Subscriber("hopper_play_sound", String, self.on_play_sound)
         rospy.Subscriber("hopper/sound/play_random", String, self.on_play_random)
         rospy.spin()
@@ -26,13 +28,13 @@ class HopperSpeechHandles(object):
         ogg_file_name = string.data + ".ogg"
         wav_file_name = string.data + ".wav"
         if path.isfile(self.primary_sound_path + ogg_file_name):
-            self.sound_client.playWave(self.primary_sound_path + ogg_file_name)
+            self.play_file(self.primary_sound_path + ogg_file_name)
         elif path.isfile(self.primary_sound_path + wav_file_name):
-            self.sound_client.playWave(self.primary_sound_path + wav_file_name)
+            self.play_file(self.primary_sound_path + wav_file_name)
         elif path.isfile(self.secondary_sound_path + ogg_file_name):
-            self.sound_client.playWave(self.secondary_sound_path + ogg_file_name)
+            self.play_file(self.secondary_sound_path + ogg_file_name)
         elif path.isfile(self.secondary_sound_path + wav_file_name):
-            self.sound_client.playWave(self.secondary_sound_path + wav_file_name)
+            self.play_file(self.secondary_sound_path + wav_file_name)
 
     def on_play_random(self, string_msg):
         folder_name = string_msg.data
@@ -40,9 +42,13 @@ class HopperSpeechHandles(object):
             folder_name = ""
         else:
             folder_name += "/"
-        sound_file_names = filter(lambda name: ".wav" in name or ".ogg" in name ,listdir(self.secondary_sound_path + folder_name))
+        sound_file_names = filter(lambda name: ".wav" in name or ".ogg" in name, listdir(self.secondary_sound_path + folder_name))
         selected_file = random.choice(sound_file_names)
-        self.sound_client.playWave(self.secondary_sound_path + folder_name + selected_file)
+        self.play_file(self.secondary_sound_path + folder_name + selected_file)
+
+    def play_file(self, path):
+        file = sound.Sound(path)
+        file.play()
 
 
 if __name__ == '__main__':

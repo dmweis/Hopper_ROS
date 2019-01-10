29fae5b4ba91d6f2305fac0e71e17e7c6d32b4a7
David Weis <davidmichaelweis@gmail.com>
2019-01-10 19:59:11 +0000

    Update morse sender

--- hopper_morse/src/morse_transmitter.py
+++ hopper_morse/src/morse_transmitter.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import sys
 import rospy
 
 from hopper_controller.msg import SingleLegCommand
@@ -24,14 +25,19 @@ MORSE = {'A': '.-',     'B': '-...',   'C': '-.-.',
         }
 
 LEG_DOWN = SingleLegCommand()
+LEG_DOWN.fast_mode = True
 LEG_DOWN.single_leg_mode_on = True
-LEG_DOWN.selected_leg = SingleLegCommand.RIGHT_FRONT
-LEG_DOWN.position.z = -0.025
+LEG_DOWN.selected_leg = SingleLegCommand.LEFT_FRONT
+LEG_DOWN.position.z = -0.02
 
 LEG_UP = SingleLegCommand()
+LEG_UP.fast_mode = True
 LEG_UP.single_leg_mode_on = True
-LEG_UP.selected_leg = SingleLegCommand.RIGHT_FRONT
-LEG_UP.position.z = -0.01
+LEG_UP.selected_leg = SingleLegCommand.LEFT_FRONT
+LEG_UP.position.z = 0
+
+NOT_SINGLE_LEG_MODE = SingleLegCommand()
+NOT_SINGLE_LEG_MODE.single_leg_mode_on = False
 
 
 class MorseEncoder(object):
@@ -39,19 +45,31 @@ class MorseEncoder(object):
         super(MorseEncoder, self).__init__()
         rospy.init_node('morse_encoder')
         self.leg_publisher = rospy.Publisher('hopper/single_leg_command', SingleLegCommand, queue_size=10)
-        self.unit = 0.2
-        self.hold_up(2)
-        # rospy.Subscriber('hopper/morse', String, self.on_morse, queue_size=1)
-        self.do_phrase("CQ")
+        self.unit = 0.1
+        self.hold_up(1)
+        #rospy.Subscriber('hopper/morse', String, self.on_morse, queue_size=1)
+        if len(sys.argv) < 2:
+            print "No argument"
+            exit()
+        try:
+            self.do_phrase(sys.argv[1])
+        except:
+            pass
         self.hold_up(0.1)
+
+    def on_morse_command(self, msg):
+        self.hold_up(1)
+        self.do_phrase(msg.data)
+        self.hold_up(0.5)
+        self.lower_leg()
 
     def do_phrase(self, phrase):
         for character in map(lambda character : MORSE[character.upper()], phrase):
             self.do_character(character)
-        self.hold_down(self.unit * 4)
+        self.hold_up(self.unit * 4)
 
     def do_character(self, character):
-        print "Doing:", character
+        print "Sending:", character
         if character == ' ':
             self.hold_up(self.unit * 7)
             return
@@ -60,6 +78,8 @@ class MorseEncoder(object):
         self.hold_up(self.unit * 3)
 
     def do_symbol(self, symbol):
+        if rospy.is_shutdown():
+            raise Exception("Interrupted")
         if symbol == '.':
             self.hold_down(self.unit)
             self.hold_up(self.unit)
@@ -76,6 +96,8 @@ class MorseEncoder(object):
         self.leg_publisher.publish(LEG_UP)
         rospy.sleep(time)
 
+    def lower_leg(self):
+        self.leg_publisher.publish(NOT_SINGLE_LEG_MODE)
 
 if __name__ == "__main__":
     MorseEncoder()

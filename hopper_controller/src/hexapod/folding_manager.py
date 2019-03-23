5cdc7c09d2368284bd25dc8224ad18ca19447ab5
David Weis <davidmichaelweis@gmail.com>
2019-03-23 03:03:03 +0000

    Move legs if they are unblocked

--- hopper_controller/src/hexapod/folding_manager.py
+++ hopper_controller/src/hexapod/folding_manager.py
@@ -3,9 +3,6 @@ import rospy
 from hopper_controller.msg import HexapodMotorPositions, LegMotorPositions
 from .hexapod_ik_driver import LegFlags
 
-a = 240
-b = 60
-
 
 def move_towards(target, current, step=0.4):
     if abs(target-current) < step:
@@ -17,16 +14,16 @@ def move_towards(target, current, step=0.4):
             return current - step, False
 
 
-def move_leg(leg, coxa=None, femur=None, tibia=None):
+def move_leg(leg, coxa=None, femur=None, tibia=None, step=0.4):
     coxa_done = True
     femur_done = True
     tibia_done = True
     if coxa:
-        leg.coxa, coxa_done = move_towards(coxa, leg.coxa)
+        leg.coxa, coxa_done = move_towards(coxa, leg.coxa, step)
     if femur:
-        leg.femur, femur_done = move_towards(femur, leg.femur)
+        leg.femur, femur_done = move_towards(femur, leg.femur, step)
     if tibia:
-        leg.tibia, tibia_done = move_towards(tibia, leg.tibia)
+        leg.tibia, tibia_done = move_towards(tibia, leg.tibia, step)
     return coxa_done and femur_done and tibia_done
 
 
@@ -61,23 +58,36 @@ class FoldingManager(object):
         right_middle_backwards = current_position.right_middle.coxa < 150.0
         while True:
             rospy.sleep(0.01)
-            self.last_motor_position.left_middle.coxa, left_done = move_towards(150, self.last_motor_position.left_middle.coxa)
-            self.last_motor_position.right_middle.coxa, right_done = move_towards(150, self.last_motor_position.right_middle.coxa)
+            lf = True
+            lr = True
+            rf = True
+            rr = True
+            if left_middle_backwards:
+                lf = move_leg(self.last_motor_position.left_front, 150)
+            lm = move_leg(self.last_motor_position.left_middle, 150)
+            if not left_middle_backwards:
+                lr = move_leg(self.last_motor_position.left_rear, 150)
+            if right_middle_backwards:
+                rf = move_leg(self.last_motor_position.right_front, 150)
+            rm = move_leg(self.last_motor_position.right_middle, 150)
+            if not right_middle_backwards:
+                rr = move_leg(self.last_motor_position.right_rear, 150)
             self.body_controller.set_motors(self.last_motor_position)
-            if left_done and right_done:
+            if lf and lm and lr and rf and rm and rr:
                 break
         while True:
             rospy.sleep(0.01)
-            self.last_motor_position.left_front.coxa, one = move_towards(150, self.last_motor_position.left_front.coxa)
-            self.last_motor_position.right_front.coxa, two = move_towards(150, self.last_motor_position.right_front.coxa)
-
-            self.last_motor_position.left_rear.coxa, three = move_towards(150, self.last_motor_position.left_rear.coxa)
-            self.last_motor_position.right_rear.coxa, four = move_towards(150, self.last_motor_position.right_rear.coxa)
+            lf = move_leg(self.last_motor_position.left_front, None, 60, 240)
+            lm = move_leg(self.last_motor_position.left_middle, None, 60, 240)
+            lr = move_leg(self.last_motor_position.left_rear, None, 60, 240)
+            rf = move_leg(self.last_motor_position.right_front, None, 240, 60)
+            rm = move_leg(self.last_motor_position.right_middle, None, 240, 60)
+            rr = move_leg(self.last_motor_position.right_rear, None, 240, 60)
             self.body_controller.set_motors(self.last_motor_position)
-            if one and two and three and four:
+            if lf and lm and lr and rf and rm and rr:
                 break
         rospy.sleep(0.05)
         self.body_controller.set_torque(False)
-        while True:
+        while not rospy.is_shutdown():
             rospy.logerr(self.body_controller.read_hexapod_motor_positions())
 

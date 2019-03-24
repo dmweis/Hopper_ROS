2dde7d2a9a93ae27d47ea220180d870531bb0db2
David Weis <davidmichaelweis@gmail.com>
2019-03-24 06:59:34 +0000

    Repeat low voltage warning if voltage too low

--- hopper_speech/src/battery_status_monitor.py
+++ hopper_speech/src/battery_status_monitor.py
@@ -14,6 +14,8 @@ SERVO_COUNT = 18
 
 
 class BatteryStatusMonitor(object):
+    critical_voltage_warning_period = rospy.Duration.from_sec(15)
+
     def __init__(self):
         super(BatteryStatusMonitor, self).__init__()
         rospy.init_node("hopper_battery_monitor", anonymous=True)
@@ -22,6 +24,7 @@ class BatteryStatusMonitor(object):
         self.first_check = True
         self.speech_publisher = rospy.Publisher('hopper_play_sound', String, queue_size=5)
         self.telemetrics_sub = rospy.Subscriber("hopper_telemetrics", HexapodTelemetrics, self.on_new_telemetrics, queue_size=1)
+        self.last_critical_voltage_warning = rospy.Time.now() + self.critical_voltage_warning_period
         rospy.spin()
 
     def on_new_telemetrics(self, message):
@@ -36,14 +39,15 @@ class BatteryStatusMonitor(object):
                 self.first_check = False
                 self.lowest_recorded_voltage = mean_voltage
                 return
-            if mean_voltage < self.lowest_recorded_voltage:
-                if mean_voltage < 10.5 and self.lowest_recorded_voltage > 10.5:
+            if mean_voltage < 10.5:
+                if self.last_critical_voltage_warning + self.critical_voltage_warning_period < rospy.Time.now():
                     self.speech_publisher.publish("battery_critical")
-                elif mean_voltage < 11 and self.lowest_recorded_voltage > 11:
-                    self.speech_publisher.publish("battery_below_11")
-                elif mean_voltage < 12 and self.lowest_recorded_voltage > 12:
-                    self.speech_publisher.publish("battery_below_12")
-                self.lowest_recorded_voltage = mean_voltage
+                    self.last_critical_voltage_warning = rospy.Time.now()
+            elif mean_voltage < 11 and self.lowest_recorded_voltage > 11:
+                self.speech_publisher.publish("battery_below_11")
+            elif mean_voltage < 12 and self.lowest_recorded_voltage > 12:
+                self.speech_publisher.publish("battery_below_12")
+            self.lowest_recorded_voltage = mean_voltage
 
 
 if __name__ == '__main__':

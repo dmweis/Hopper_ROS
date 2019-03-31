8f927150155af11afb9630b8476d2bc8acd91b96
David Weis <davidmichaelweis@gmail.com>
2019-03-31 15:02:00 +0100

    Fix repeat low voltage messages and add repeat warning for critical

--- hopper_speech/src/battery_status_monitor.py
+++ hopper_speech/src/battery_status_monitor.py
@@ -39,13 +39,13 @@ class BatteryStatusMonitor(object):
                 self.first_check = False
                 self.lowest_recorded_voltage = mean_voltage
                 return
-            if mean_voltage < 10.5:
+            if mean_voltage < 10.5 or self.lowest_recorded_voltage < 10.5:
                 if self.last_critical_voltage_warning + self.critical_voltage_warning_period < rospy.Time.now():
                     self.speech_publisher.publish("battery_critical")
                     self.last_critical_voltage_warning = rospy.Time.now()
-            elif mean_voltage < 11 and self.lowest_recorded_voltage > 11:
+            elif mean_voltage < 11 and self.lowest_recorded_voltage >= 11:
                 self.speech_publisher.publish("battery_below_11")
-            elif mean_voltage < 12 and self.lowest_recorded_voltage > 12:
+            elif mean_voltage < 12 and self.lowest_recorded_voltage >= 12:
                 self.speech_publisher.publish("battery_below_12")
             self.lowest_recorded_voltage = mean_voltage
 

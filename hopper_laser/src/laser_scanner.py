98fe820ac71fc8cccc888ee42db403734e353a4d
David Weis <davidmichaelweis@gmail.com>
2019-04-28 22:10:06 +0100

    Try both x and y tilt

--- hopper_laser/src/laser_scanner.py
+++ hopper_laser/src/laser_scanner.py
@@ -4,7 +4,7 @@ from __future__ import division
 
 import rospy
 
-from math import cos, pi, radians
+from math import cos, sin, pi, radians
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import PointCloud2
 from std_msgs.msg import Bool, Float32
@@ -40,8 +40,8 @@ class LaserScanner(object):
                 scan_start = rospy.get_time()
                 max_tilt_angle = -max_tilt_angle
             current_progress = (rospy.get_time() - scan_start) / self.scan_time
-            current_cos = cos(current_progress * pi)
-            self.robot_pose.angular.y = max_tilt_angle * current_cos
+            self.robot_pose.angular.y = max_tilt_angle * cos(current_progress * pi)
+            self.robot_pose.angular.x = max_tilt_angle * sin(current_progress * pi)
             self.stance_translate.publish(self.robot_pose)
             update_rate.sleep()
 
@@ -55,8 +55,8 @@ class LaserScanner(object):
         if not self.scanner_active:
             rospy.sleep(0.5)
             self.robot_pose.angular.y = 0
+            self.robot_pose.angular.x = 0
             self.stance_translate.publish(self.robot_pose)
-
 
     def on_scanner_time(self, msg):
         self.scan_time = msg.data

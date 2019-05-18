9a9340904c84b827643ca909e27653ad9344dfd2
David Weis <davidmichaelweis@gmail.com>
2019-05-18 18:43:55 +0100

    Add r2d2 speech mode

---
+++ hopper_speech/src/r2d2_speak.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import rospy
+import random
+
+from ttastromech import TTAstromech
+from std_msgs.msg import String
+
+
+class R2D2Speech(object):
+    def __init__(self):
+        super(R2D2Speech, self).__init__()
+        self.speaker = TTAstromech()
+        rospy.init_node("hopper_r2d2_speech_core", anonymous=True)
+        rospy.Subscriber("hopper_play_sound", String, self.on_play_sound)
+        rospy.Subscriber("hopper/sound/play_random", String, self.on_play_sound)
+        rospy.spin()
+
+    def on_play_sound(self, string):
+        self.speaker.speak(string.data)
+
+
+if __name__ == '__main__':
+    R2D2Speech()

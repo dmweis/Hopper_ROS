7738be7de7b3cc1278cf7159588a08aabb600aaf
David Weis <davidmichaelweis@gmail.com>
2019-07-16 19:55:53 +0100

    Fix wrong reference

--- hopper_controller/src/ros_abstraction/lidar_controller.py
+++ hopper_controller/src/ros_abstraction/lidar_controller.py
@@ -4,7 +4,7 @@ from std_srvs.srv import Empty
 
 class LidarController(object):
     def __init__(self):
-        super(SoundPlayer, self).__init__()
+        super(LidarController, self).__init__()
         self.lidar_on = rospy.ServiceProxy("motor_start", Empty)
         self.lidar_off = rospy.ServiceProxy("motor_stop", Empty)
 

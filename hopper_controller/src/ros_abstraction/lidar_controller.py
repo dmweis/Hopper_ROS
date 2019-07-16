9f92f28e0ce8692c7db8cd10cc93ac4b8c5b2e0d
David Weis <davidmichaelweis@gmail.com>
2019-07-16 19:57:10 +0100

    Fix wrong service name

--- hopper_controller/src/ros_abstraction/lidar_controller.py
+++ hopper_controller/src/ros_abstraction/lidar_controller.py
@@ -5,8 +5,8 @@ from std_srvs.srv import Empty
 class LidarController(object):
     def __init__(self):
         super(LidarController, self).__init__()
-        self.lidar_on = rospy.ServiceProxy("motor_start", Empty)
-        self.lidar_off = rospy.ServiceProxy("motor_stop", Empty)
+        self.lidar_on = rospy.ServiceProxy("start_motor", Empty)
+        self.lidar_off = rospy.ServiceProxy("stop_motor", Empty)
 
     def start(self):
         self.lidar_on()

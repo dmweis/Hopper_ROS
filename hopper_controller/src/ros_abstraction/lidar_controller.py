83a275efa5096b62ef8ea755ec6cda56124cc044
David Weis <davidmichaelweis@gmail.com>
2019-07-16 19:54:41 +0100

    Fix bug with asking for useless arguemnt

--- hopper_controller/src/ros_abstraction/lidar_controller.py
+++ hopper_controller/src/ros_abstraction/lidar_controller.py
@@ -3,7 +3,7 @@ from std_srvs.srv import Empty
 
 
 class LidarController(object):
-    def __init__(self, sound_on):
+    def __init__(self):
         super(SoundPlayer, self).__init__()
         self.lidar_on = rospy.ServiceProxy("motor_start", Empty)
         self.lidar_off = rospy.ServiceProxy("motor_stop", Empty)

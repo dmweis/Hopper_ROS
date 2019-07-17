7d5eada2882ff2b0afd264b17fe3d13ad4a65a67
David Weis <davidmichaelweis@gmail.com>
2019-07-17 03:05:36 +0100

    Add high five node

---
+++ hopper_climber/src/hopper_climber/high_fiver.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+from __future__ import division
+
+import rospy
+import numpy as np
+
+from math import radians, ceil, degrees, pi, cos, sin
+from sensor_msgs.msg import LaserScan
+from visualization_msgs.msg import Marker
+
+
+def index_to_heading(index, angle_min, angle_increment):
+    return angle_min + index * angle_increment
+
+def heading_to_index(heading, angle_min, angle_increment):
+    return max(int(ceil((heading - angle_min) / angle_increment)), 0)
+
+class HighFiveController(object):
+    def __init__(self):
+        super(HighFiveController, self).__init__()
+        rospy.init_node("high_five_controller")
+        # variables
+        self.left_min_angle_limit = -pi
+        self.left_max_angle_limit = -pi + radians(60)
+        self.right_min_angle_limit = pi - radians(60)
+        self.right_max_angle_limit = pi
+        self.max_distance = 0.4
+        # Subscribers
+        rospy.Subscriber("scan", LaserScan, self.on_laser_msg, queue_size=1)
+        self.marker_publisher = rospy.Publisher("hand_marker", Marker, queue_size=10)
+        rospy.spin()
+
+    def on_laser_msg(self, scan_msg):
+        left_start_index = heading_to_index(self.left_min_angle_limit, scan_msg.angle_min, scan_msg.angle_increment)
+        left_stop_index = heading_to_index(self.left_max_angle_limit, scan_msg.angle_min, scan_msg.angle_increment)
+        success, x, y = self.detect_hand(left_start_index, left_stop_index, scan_msg)
+        if success:
+            self.display_marker(x, y)
+        right_start_index = heading_to_index(self.right_min_angle_limit, scan_msg.angle_min, scan_msg.angle_increment)
+        right_stop_index = heading_to_index(self.right_max_angle_limit, scan_msg.angle_min, scan_msg.angle_increment)
+        success, x, y = self.detect_hand(right_start_index, right_stop_index, scan_msg)
+        if success:
+            self.display_marker(x, y)
+    
+    def detect_hand(self, start_index, stop_index, scan_msg):
+        detected_points = map(lambda dist: dist < self.max_distance, scan_msg.ranges[start_index:stop_index])
+        if True not in detected_points:
+            return False, 0, 0
+        detected_groups = []
+        tmp_start_point = None
+        for i in xrange(len(detected_points)):
+            if detected_points[i]:
+                if tmp_start_point is None:
+                    tmp_start_point = i
+                    continue
+            else:
+                if tmp_start_point is not None:
+                    detected_groups.append((tmp_start_point, i-1))
+                    tmp_start_point = None
+        if tmp_start_point is not None:
+            detected_groups.append((tmp_start_point, len(detected_points)-1))
+        group_widths = map(lambda (start, stop): abs(stop-start), detected_groups)
+        # TODO: less ugly
+        largest_group = detected_groups[group_widths.index(max(group_widths))]
+        index_middle_largest_group = int((largest_group[0] + largest_group[1]) / 2) + start_index
+        heading = index_to_heading(index_middle_largest_group, scan_msg.angle_min, scan_msg.angle_increment)
+        distance = scan_msg.ranges[index_middle_largest_group]
+
+        x = distance * cos(heading)
+        y = distance * sin(heading)
+
+        return True, x, y
+
+    def display_marker(self, x, y):
+        marker = Marker()
+        marker.header.frame_id = "laser"
+        marker.header.stamp = rospy.Time()
+        marker.type = Marker.SPHERE
+        marker.action = Marker.ADD
+        marker.pose.orientation.w = 1.
+        marker.pose.position.x = x
+        marker.pose.position.y = y
+        marker.scale.x = 0.1
+        marker.scale.y = 0.1
+        marker.scale.z = 0.1
+        marker.color.a = 1.0
+        marker.color.r = 0.0
+        marker.color.g = 1.0
+        marker.color.b = 0.0
+        marker.lifetime = rospy.Duration(0)
+        marker.frame_locked = True
+        self.marker_publisher.publish(marker)
+
+
+if __name__ == "__main__":
+    HighFiveController()

1a3f1c35a01f03ee20b2452424f72956d5655a80
David Weis <davidmichaelweis@gmail.com>
2019-07-22 04:16:55 +0100

    Add marker for obstacle detected

--- hopper_obstacle_detector/src/obstacle_detector.cpp
+++ hopper_obstacle_detector/src/obstacle_detector.cpp
@@ -7,13 +7,14 @@
 #include "octomap/math/Vector3.h"
 #include <tf2_ros/transform_listener.h>
 #include <geometry_msgs/TransformStamped.h>
+#include <visualization_msgs/Marker.h>
 
 using namespace std;
 using namespace octomap;
 
-
 tf2_ros::Buffer *globalTfBuffer;
 tf2_ros::TransformListener *globalTfListener;
+ros::Publisher *global_marker_pub;
 
 void octomapCallback(const octomap_msgs::Octomap &msg)
 {
@@ -49,9 +50,9 @@ void octomapCallback(const octomap_msgs::Octomap &msg)
         float qu = transformStamped.transform.rotation.w;
         octomath::Quaternion rotation(qu, qx, qy, qz);
 
-        octomath::Vector3 front = octomath::Vector3(1, 0, 0);
+        octomath::Vector3 front = octomath::Vector3(-1, 0, 0);
         octomath::Vector3 b = rotation.rotate(front);
-        octomath::Vector3 heading(b.x(), b.y(), 0.0);
+        octomath::Vector3 heading(b.x(), b.y(), b.z());
         octomath::Vector3 result = octomath::Vector3(0, 0, 0);
         std::cout << "origin " << a << std::endl;
         std::cout << "direction " << heading << std::endl;
@@ -63,11 +64,71 @@ void octomapCallback(const octomap_msgs::Octomap &msg)
             if (n)
             {
                 std::cout << "Value: " << n->getValue() << "\n";
+                // Create marker
+                visualization_msgs::Marker marker;
+                marker.header.frame_id = "/odom";
+                marker.header.stamp = ros::Time::now();
+                marker.ns = "basic_shapes";
+                marker.id = 0;
+                marker.type = visualization_msgs::Marker::SPHERE;
+                marker.action = visualization_msgs::Marker::ADD;
+
+                marker.pose.position.x = result.x();
+                marker.pose.position.y = result.y();
+                marker.pose.position.z = result.z();
+                marker.pose.orientation.x = 0.0;
+                marker.pose.orientation.y = 0.0;
+                marker.pose.orientation.z = 0.0;
+                marker.pose.orientation.w = 1.0;
+
+                // Set the scale of the marker -- 1x1x1 here means 1m on a side
+                marker.scale.x = 0.5;
+                marker.scale.y = 0.5;
+                marker.scale.z = 0.5;
+
+                // Set the color -- be sure to set alpha to something non-zero!
+                marker.color.r = 1.0f;
+                marker.color.g = 0.0f;
+                marker.color.b = 0.0f;
+                marker.color.a = 1.0;
+
+                marker.lifetime = ros::Duration(10);
+
+                global_marker_pub->publish(marker);
             }
         }
         else
         {
-            // ROS_INFO("Not found thingy");
+            // visualization_msgs::Marker marker;
+            // marker.header.frame_id = "/odom";
+            // marker.header.stamp = ros::Time::now();
+            // marker.ns = "basic_shapes";
+            // marker.id = 0;
+            // marker.type = visualization_msgs::Marker::CUBE;
+            // marker.action = visualization_msgs::Marker::DELETE;
+
+            // marker.pose.position.x = 0;
+            // marker.pose.position.y = 0;
+            // marker.pose.position.z = 0;
+            // marker.pose.orientation.x = 0.0;
+            // marker.pose.orientation.y = 0.0;
+            // marker.pose.orientation.z = 0.0;
+            // marker.pose.orientation.w = 1.0;
+
+            // // Set the scale of the marker -- 1x1x1 here means 1m on a side
+            // marker.scale.x = 0.5;
+            // marker.scale.y = 0.5;
+            // marker.scale.z = 0.5;
+
+            // // Set the color -- be sure to set alpha to something non-zero!
+            // marker.color.r = 0.0f;
+            // marker.color.g = 1.0f;
+            // marker.color.b = 1.0f;
+            // marker.color.a = 1.0;
+
+            // marker.lifetime = ros::Duration();
+
+            // global_marker_pub->publish(marker);
         }
 
         // double v = 0;
@@ -105,7 +166,8 @@ int main(int argc, char **argv)
     globalTfListener = &tfListener;
 
     ros::Subscriber sub = n.subscribe("/global/octomap_full", 1000, octomapCallback);
-
+    ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("hopper_obstacle_detected", 1);
+    global_marker_pub = &marker_pub;
     ros::spin();
 
     return 0;

20c0b59aeb970315ab33c402e32e22e2a7a9a2d9
David Weis <davidmichaelweis@gmail.com>
2019-07-23 21:04:30 +0100

    Make obstacle detector detect at layers

--- hopper_obstacle_detector/src/obstacle_detector.cpp
+++ hopper_obstacle_detector/src/obstacle_detector.cpp
@@ -7,7 +7,10 @@
 #include "octomap/math/Vector3.h"
 #include <tf2_ros/transform_listener.h>
 #include <geometry_msgs/TransformStamped.h>
+#include <geometry_msgs/Point32.h>
 #include <visualization_msgs/Marker.h>
+#include <sensor_msgs/PointCloud.h>
+#include <sensor_msgs/ChannelFloat32.h>
 
 using namespace std;
 using namespace octomap;
@@ -15,8 +18,53 @@ using namespace octomap;
 tf2_ros::Buffer *globalTfBuffer;
 tf2_ros::TransformListener *globalTfListener;
 ros::Publisher *global_marker_pub;
+ros::Publisher *global_collision_pub;
+int globalMarkerCounter;
 
-void octomapCallback(const octomap_msgs::Octomap &msg)
+void displayMarker(octomath::Vector3 *vector)
+{
+    visualization_msgs::Marker marker;
+    marker.header.frame_id = "/odom";
+    marker.header.stamp = ros::Time();
+    marker.ns = "obstacle_markers";
+    marker.id = globalMarkerCounter++;
+    marker.action = visualization_msgs::Marker::ADD;
+    marker.type = visualization_msgs::Marker::SPHERE;
+
+    marker.pose.position.x = vector->x();
+    marker.pose.position.y = vector->y();
+    marker.pose.position.z = vector->z();
+    marker.pose.orientation.w = 1.0;
+
+    // Set the scale of the marker -- 1x1x1 here means 1m on a side
+    marker.scale.x = 0.1;
+    marker.scale.y = 0.1;
+    marker.scale.z = 0.1;
+
+    // Set the color -- be sure to set alpha to something non-zero!
+    marker.color.r = 1.0f;
+    marker.color.g = 0.0f;
+    marker.color.b = 0.0f;
+    marker.color.a = 1.0;
+
+    marker.lifetime = ros::Duration(10);
+
+    global_marker_pub->publish(marker);
+}
+
+void deleteMarker()
+{
+    visualization_msgs::Marker marker;
+    marker.header.frame_id = "/odom";
+    marker.header.stamp = ros::Time();
+    marker.ns = "obstacle_markers";
+    marker.id = 0;
+    marker.action = visualization_msgs::Marker::DELETEALL;
+
+    global_marker_pub->publish(marker);
+}
+
+void octomapCallback(const octomap_msgs::Octomap msg)
 {
     // ROS_INFO("Hi [%s]", msg.header.frame_id.c_str());
     octomap::AbstractOcTree *tree = octomap_msgs::fullMsgToMap(msg);
@@ -25,12 +73,11 @@ void octomapCallback(const octomap_msgs::Octomap &msg)
     {
         geometry_msgs::TransformStamped transformStamped;
 
-        while (true)
+        do
         {
             try
             {
-                transformStamped = globalTfBuffer->lookupTransform("odom", "body_link",
-                                                                   ros::Time(0));
+                transformStamped = globalTfBuffer->lookupTransform("odom", "body_link", ros::Time(0));
                 break;
             }
             catch (tf2::TransformException &ex)
@@ -38,97 +85,62 @@ void octomapCallback(const octomap_msgs::Octomap &msg)
                 ROS_WARN("%s", ex.what());
                 ros::Duration(1.0).sleep();
             }
-        }
+        } while (ros::ok());
         float x = transformStamped.transform.translation.x;
         float y = transformStamped.transform.translation.y;
         float z = transformStamped.transform.translation.z;
-        octomath::Vector3 a = octomath::Vector3(x, y, z);
+
+        octomath::Vector3 position = octomath::Vector3(x, y, z);
 
         float qx = transformStamped.transform.rotation.x;
         float qy = transformStamped.transform.rotation.y;
         float qz = transformStamped.transform.rotation.z;
         float qu = transformStamped.transform.rotation.w;
+
         octomath::Quaternion rotation(qu, qx, qy, qz);
 
-        octomath::Vector3 front = octomath::Vector3(-1, 0, 0);
-        octomath::Vector3 b = rotation.rotate(front);
-        octomath::Vector3 heading(b.x(), b.y(), b.z());
+        octomath::Vector3 forward = octomath::Vector3(1, 0, 0);
+        octomath::Vector3 orientation = rotation.rotate(forward);
+
+        octomath::Vector3 heading(orientation.x(), orientation.y(), 0);
         octomath::Vector3 result = octomath::Vector3(0, 0, 0);
-        std::cout << "origin " << a << std::endl;
-        std::cout << "direction " << heading << std::endl;
-        std::cout << "Result " << result << std::endl;
-        if (octTree->castRay(a, heading, result, true, 10))
+
+        bool pointsDetected = false;
+        sensor_msgs::PointCloud resultCloud;
+        resultCloud.header.frame_id = "odom";
+        resultCloud.header.stamp = ros::Time();
+        for (float height = 0.0; height <= 0.2; height += 0.01)
         {
-            ROS_INFO("Found thingy!");
-            OcTreeNode *n = octTree->search(result);
-            if (n)
+            octomath::Vector3 origin(position.x(), position.y(), height);
+            if (octTree->castRay(origin, heading, result, true, 10))
             {
-                std::cout << "Value: " << n->getValue() << "\n";
-                // Create marker
-                visualization_msgs::Marker marker;
-                marker.header.frame_id = "/odom";
-                marker.header.stamp = ros::Time::now();
-                marker.ns = "basic_shapes";
-                marker.id = 0;
-                marker.type = visualization_msgs::Marker::SPHERE;
-                marker.action = visualization_msgs::Marker::ADD;
+                pointsDetected = true;
+                ROS_INFO("Found thingy!");
+                OcTreeNode *hitNode = octTree->search(result);
+                if (hitNode)
+                {
+                    // std::cout << "Value: " << hitNode->getValue() << "\n";
+                    displayMarker(&result);
+                    geometry_msgs::Point32 hitCoordinates;
+                    hitCoordinates.x = result.x();
+                    hitCoordinates.y = result.y();
+                    hitCoordinates.z = result.z();
+                    resultCloud.points.push_back(hitCoordinates);
 
-                marker.pose.position.x = result.x();
-                marker.pose.position.y = result.y();
-                marker.pose.position.z = result.z();
-                marker.pose.orientation.x = 0.0;
-                marker.pose.orientation.y = 0.0;
-                marker.pose.orientation.z = 0.0;
-                marker.pose.orientation.w = 1.0;
-
-                // Set the scale of the marker -- 1x1x1 here means 1m on a side
-                marker.scale.x = 0.5;
-                marker.scale.y = 0.5;
-                marker.scale.z = 0.5;
-
-                // Set the color -- be sure to set alpha to something non-zero!
-                marker.color.r = 1.0f;
-                marker.color.g = 0.0f;
-                marker.color.b = 0.0f;
-                marker.color.a = 1.0;
-
-                marker.lifetime = ros::Duration(10);
-
-                global_marker_pub->publish(marker);
+                    sensor_msgs::ChannelFloat32 channelData;
+                    channelData.name = "intensity";
+                    channelData.values.push_back(hitNode->getValue());
+                    resultCloud.channels.push_back(channelData);
+                }
             }
+        }
+        if (pointsDetected)
+        {
+            global_collision_pub->publish(resultCloud);
         }
         else
         {
-            // visualization_msgs::Marker marker;
-            // marker.header.frame_id = "/odom";
-            // marker.header.stamp = ros::Time::now();
-            // marker.ns = "basic_shapes";
-            // marker.id = 0;
-            // marker.type = visualization_msgs::Marker::CUBE;
-            // marker.action = visualization_msgs::Marker::DELETE;
-
-            // marker.pose.position.x = 0;
-            // marker.pose.position.y = 0;
-            // marker.pose.position.z = 0;
-            // marker.pose.orientation.x = 0.0;
-            // marker.pose.orientation.y = 0.0;
-            // marker.pose.orientation.z = 0.0;
-            // marker.pose.orientation.w = 1.0;
-
-            // // Set the scale of the marker -- 1x1x1 here means 1m on a side
-            // marker.scale.x = 0.5;
-            // marker.scale.y = 0.5;
-            // marker.scale.z = 0.5;
-
-            // // Set the color -- be sure to set alpha to something non-zero!
-            // marker.color.r = 0.0f;
-            // marker.color.g = 1.0f;
-            // marker.color.b = 1.0f;
-            // marker.color.a = 1.0;
-
-            // marker.lifetime = ros::Duration();
-
-            // global_marker_pub->publish(marker);
+            deleteMarker();
         }
 
         // double v = 0;
@@ -151,6 +163,7 @@ void octomapCallback(const octomap_msgs::Octomap &msg)
 
         // std::cout<<"VOLUME::::"<<v<<endl;
         // std::cout << count <<endl;
+        delete octTree;
     }
 }
 
@@ -162,12 +175,16 @@ int main(int argc, char **argv)
 
     tf2_ros::Buffer tfBuffer;
     globalTfBuffer = &tfBuffer;
-    tf2_ros::TransformListener tfListener(tfBuffer);
+    tf2_ros::TransformListener tfListener(*globalTfBuffer);
     globalTfListener = &tfListener;
 
     ros::Subscriber sub = n.subscribe("/global/octomap_full", 1000, octomapCallback);
-    ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("hopper_obstacle_detected", 1);
+    ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("hopper/obstacle_detected/markers", 10);
+    ros::Publisher collisionPublisher = n.advertise<sensor_msgs::PointCloud>("hopper/detected_obstacles", 10);
+
+    global_collision_pub = &collisionPublisher;
     global_marker_pub = &marker_pub;
+
     ros::spin();
 
     return 0;

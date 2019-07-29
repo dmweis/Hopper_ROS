2391fe02f13f6e2f57eae9d0be8c57b04086b4e3
David Weis <davidmichaelweis@gmail.com>
2019-07-30 00:24:33 +0100

    Add obstacle caster service

---
+++ hopper_climber/src/hopper_climber/ray_cast_tester.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+from __future__ import division
+
+import rospy
+
+from hopper_obstacle_detector.srv import CastRay, CastRayRequest
+from geometry_msgs.msg import Vector3, Vector3Stamped
+from visualization_msgs.msg import Marker
+
+
+class RayCastTester(object):
+    def __init__(self):
+        super(RayCastTester, self).__init__()
+        rospy.init_node("blind_climb_controller")
+        rospy.wait_for_service("hopper/detected_obstacles/cast_ray")
+        self.cast_ray = rospy.ServiceProxy("hopper/detected_obstacles/cast_ray", CastRay)
+        self.marker_publisher = rospy.Publisher("hopper/detected_obstacles/cast_ray/markers", Marker, queue_size=10, latch=True)
+
+        request = CastRayRequest()
+        direction = Vector3(1, 0, 0)
+        origin = Vector3Stamped()
+        origin.header.frame_id = "body_link"
+        origin.vector = Vector3(0, 0, 0)
+        request.origins.append(origin)
+        request.directions.append(direction)
+        response = self.cast_ray(request)
+        
+        for i in range(len(response.hits)):
+            if response.hits[i]:
+                self.display_marker(response.targets[i].vector.x, response.targets[i].vector.y, response.targets[i].vector.z)
+        rospy.spin()
+
+    def display_marker(self, x, y, z):
+        marker = Marker()
+        marker.header.frame_id = "body_link"
+        marker.header.stamp = rospy.Time()
+        marker.type = Marker.SPHERE
+        marker.action = Marker.ADD
+        marker.pose.orientation.w = 1.
+        marker.pose.position.x = x
+        marker.pose.position.y = y
+        marker.scale.x = 0.1
+        marker.scale.y = 0.1
+        marker.scale.z = 0.1
+        marker.color.a = 1.0
+        marker.color.r = 0.0
+        marker.color.g = 1.0
+        marker.color.b = 0.0
+        marker.lifetime = rospy.Duration(0)
+        marker.frame_locked = True
+        self.marker_publisher.publish(marker)
+
+if __name__ == "__main__":
+    RayCastTester()
--- hopper_obstacle_detector/src/obstacle_detector.cpp
+++ hopper_obstacle_detector/src/obstacle_detector.cpp
@@ -11,15 +11,98 @@
 #include <visualization_msgs/Marker.h>
 #include <sensor_msgs/PointCloud.h>
 #include <sensor_msgs/ChannelFloat32.h>
+#include <hopper_obstacle_detector/CastRay.h>
+
+#include <geometry_msgs/Vector3Stamped.h>
 
 using namespace std;
 using namespace octomap;
+using namespace octomath;
 
 tf2_ros::Buffer *globalTfBuffer;
 tf2_ros::TransformListener *globalTfListener;
 ros::Publisher *global_marker_pub;
 ros::Publisher *global_collision_pub;
+ros::Publisher *global_obstacle_edge_pub;
 int globalMarkerCounter;
+
+octomap::OcTree *last_octmap_cloud = NULL;
+
+void getTransform(const char *from_frame, const char *to_frame, Vector3 &position, Quaternion &rotation)
+{
+    geometry_msgs::TransformStamped transformStamped;
+    do
+    {
+        try
+        {
+            transformStamped = globalTfBuffer->lookupTransform(from_frame, to_frame, ros::Time(0));
+            break;
+        }
+        catch (tf2::TransformException &ex)
+        {
+            ROS_WARN("%s", ex.what());
+            ros::Duration(1.0).sleep();
+        }
+    } while (ros::ok());
+    float x = transformStamped.transform.translation.x;
+    float y = transformStamped.transform.translation.y;
+    float z = transformStamped.transform.translation.z;
+
+    position = Vector3(x, y, z);
+
+    float qx = transformStamped.transform.rotation.x;
+    float qy = transformStamped.transform.rotation.y;
+    float qz = transformStamped.transform.rotation.z;
+    float qu = transformStamped.transform.rotation.w;
+
+    rotation = Quaternion(qu, qx, qy, qz);
+}
+
+bool castRay(hopper_obstacle_detector::CastRay::Request &req, hopper_obstacle_detector::CastRay::Response &res)
+{
+    for (int i = 0; i < req.origins.size(); i++)
+    {
+        Vector3 frame_translation;
+        Quaternion rotation;
+        getTransform("odom", req.origins[i].header.frame_id.c_str(), frame_translation, rotation);
+
+        Vector3 direction(req.directions[i].x, req.directions[i].y, req.directions[i].z);
+        direction = rotation.rotate(direction);
+
+        Vector3 position(req.origins[i].vector.x, req.origins[i].vector.y, req.origins[i].vector.z);
+        position = rotation.rotate(position);
+        position += frame_translation;
+
+        Vector3 result;
+
+        geometry_msgs::Vector3Stamped resultHit;
+        resultHit.header.frame_id = req.origins[i].header.frame_id;
+        bool hitDetected = false;
+        cout << "cating" << endl;
+        cout << frame_translation << endl;
+        if (last_octmap_cloud->castRay(position, direction, result, true, 10))
+        {
+            cout << "cast worked" << endl;
+
+            OcTreeNode *hitNode = last_octmap_cloud->search(result);
+            if (hitNode)
+            {
+                hitDetected = true;
+                Vector3 translatedResult;
+                translatedResult = result - frame_translation;
+                Quaternion invertedRotation = rotation.inv();
+                translatedResult = invertedRotation.rotate(translatedResult);
+                resultHit.vector.x = translatedResult.x();
+                resultHit.vector.y = translatedResult.y();
+                resultHit.vector.z = translatedResult.z();
+            }
+        }
+
+        res.hits.push_back(hitDetected);
+        res.targets.push_back(resultHit);
+    }
+    return true;
+}
 
 void displayMarker(octomath::Vector3 *vector)
 {
@@ -69,15 +152,17 @@ void octomapCallback(const octomap_msgs::Octomap msg)
     // ROS_INFO("Hi [%s]", msg.header.frame_id.c_str());
     octomap::AbstractOcTree *tree = octomap_msgs::fullMsgToMap(msg);
     octomap::OcTree *octTree = dynamic_cast<octomap::OcTree *>(tree);
+
+    // delete last_octmap_cloud;
+    last_octmap_cloud = octTree;
     if (octTree)
     {
         geometry_msgs::TransformStamped transformStamped;
-
         do
         {
             try
             {
-                transformStamped = globalTfBuffer->lookupTransform("odom", "body_link", ros::Time(0));
+                transformStamped = globalTfBuffer->lookupTransform("odom", "base_footprint", ros::Time(0));
                 break;
             }
             catch (tf2::TransformException &ex)
@@ -90,7 +175,7 @@ void octomapCallback(const octomap_msgs::Octomap msg)
         float y = transformStamped.transform.translation.y;
         float z = transformStamped.transform.translation.z;
 
-        octomath::Vector3 position = octomath::Vector3(x, y, z);
+        Vector3 position = octomath::Vector3(x, y, z);
 
         float qx = transformStamped.transform.rotation.x;
         float qy = transformStamped.transform.rotation.y;
@@ -103,40 +188,104 @@ void octomapCallback(const octomap_msgs::Octomap msg)
         octomath::Vector3 orientation = rotation.rotate(forward);
 
         octomath::Vector3 heading(orientation.x(), orientation.y(), 0);
-        octomath::Vector3 result = octomath::Vector3(0, 0, 0);
 
         bool pointsDetected = false;
         sensor_msgs::PointCloud resultCloud;
         resultCloud.header.frame_id = "odom";
         resultCloud.header.stamp = ros::Time();
-        for (float height = 0.0; height <= 0.2; height += 0.01)
+
+        vector<float> distances;
+        vector<octomath::Vector3 *> hits;
+        for (float height = 0.0; height <= 0.4; height += 0.01)
         {
+            octomath::Vector3 *result = new octomath::Vector3(0, 0, 0);
             octomath::Vector3 origin(position.x(), position.y(), height);
-            if (octTree->castRay(origin, heading, result, true, 10))
+            if (octTree->castRay(origin, heading, *result, true, 10))
             {
                 pointsDetected = true;
-                ROS_INFO("Found thingy!");
-                OcTreeNode *hitNode = octTree->search(result);
+                // ROS_INFO("Found thingy!");
+                OcTreeNode *hitNode = octTree->search(*result);
                 if (hitNode)
                 {
+
                     // std::cout << "Value: " << hitNode->getValue() << "\n";
-                    displayMarker(&result);
+                    displayMarker(result);
                     geometry_msgs::Point32 hitCoordinates;
-                    hitCoordinates.x = result.x();
-                    hitCoordinates.y = result.y();
-                    hitCoordinates.z = result.z();
+                    hitCoordinates.x = result->x();
+                    hitCoordinates.y = result->y();
+                    hitCoordinates.z = result->z();
                     resultCloud.points.push_back(hitCoordinates);
 
                     sensor_msgs::ChannelFloat32 channelData;
-                    channelData.name = "intensity";
-                    channelData.values.push_back(hitNode->getValue());
+                    channelData.name = "distance";
+                    // channelData.values.push_back(hitNode->getValue());
+                    channelData.values.push_back(origin.distanceXY(*result));
                     resultCloud.channels.push_back(channelData);
+
+                    hits.push_back(result);
+                    distances.push_back(origin.distanceXY(*result));
                 }
+                else
+                {
+                    delete result;
+                }
+                delete hitNode;
             }
         }
+
+        sensor_msgs::PointCloud obstacleEdgeCloud;
+        obstacleEdgeCloud.header.frame_id = "odom";
+        obstacleEdgeCloud.header.stamp = ros::Time();
+
+        // obstacle detected
+        if (hits.size() > 0)
+        {
+            float distanceVariation = 0.01;
+            float lastObstacledistance = distances[0];
+            vector<octomath::Vector3 *> obstacleEdges;
+            for (int i = 0; i < hits.size(); i++)
+            {
+                float height = hits[0]->z();
+                float distance = distances[i];
+                if (abs(lastObstacledistance - distance) > distanceVariation)
+                {
+                    if (i > 0)
+                    {
+                        obstacleEdges.push_back(hits[i - 1]);
+                    }
+                }
+                lastObstacledistance = distance;
+                if (i == hits.size() - 1)
+                {
+                    obstacleEdges.push_back(hits[i]);
+                }
+            }
+            // print data about obstacles
+            for (int i = 0; i < obstacleEdges.size(); i++)
+            {
+                geometry_msgs::Point32 hitCoordinates;
+                hitCoordinates.x = obstacleEdges[i]->x();
+                hitCoordinates.y = obstacleEdges[i]->y();
+                hitCoordinates.z = obstacleEdges[i]->z();
+                obstacleEdgeCloud.points.push_back(hitCoordinates);
+
+                sensor_msgs::ChannelFloat32 channelData;
+                channelData.name = "height";
+                channelData.values.push_back(obstacleEdges[i]->z() - position.z());
+                obstacleEdgeCloud.channels.push_back(channelData);
+
+                // cout << "Obstacle height " << obstacleEdges[i]->z() - position.z() << endl;
+            }
+        }
+        // cout << endl;
+        // clear memory
+        // for (int i = 0; i < hits.size(); i++){
+        //     delete hits[i];
+        // }
         if (pointsDetected)
         {
             global_collision_pub->publish(resultCloud);
+            global_obstacle_edge_pub->publish(obstacleEdgeCloud);
         }
         else
         {
@@ -163,7 +312,7 @@ void octomapCallback(const octomap_msgs::Octomap msg)
 
         // std::cout<<"VOLUME::::"<<v<<endl;
         // std::cout << count <<endl;
-        delete octTree;
+        // delete octTree;
     }
 }
 
@@ -178,12 +327,16 @@ int main(int argc, char **argv)
     tf2_ros::TransformListener tfListener(*globalTfBuffer);
     globalTfListener = &tfListener;
 
-    ros::Subscriber sub = n.subscribe("/global/octomap_full", 1000, octomapCallback);
-    ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("hopper/obstacle_detected/markers", 10);
+    ros::Subscriber sub = n.subscribe("/local/octomap_full", 1000, octomapCallback);
+    ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("hopper/detected_obstacles/markers", 10);
     ros::Publisher collisionPublisher = n.advertise<sensor_msgs::PointCloud>("hopper/detected_obstacles", 10);
+    ros::Publisher obstacleEdges = n.advertise<sensor_msgs::PointCloud>("hopper/detected_obstacles/edges", 10);
+
+    ros::ServiceServer service = n.advertiseService("hopper/detected_obstacles/cast_ray", castRay);
 
     global_collision_pub = &collisionPublisher;
     global_marker_pub = &marker_pub;
+    global_obstacle_edge_pub = &obstacleEdges;
 
     ros::spin();
 
